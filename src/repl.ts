4ba35cc8d29b42e6d35c99e63ca47e686a811d13
LeonelCedillo <leonel.cs.979@gmail.com>
2025-11-03 17:00:23 -0800

    progress

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,4 @@
+import readline from "readline";
 
 export function cleanInput(input: string): string[] {
     return input
@@ -5,4 +6,24 @@ export function cleanInput(input: string): string[] {
         .trim()
         .split(" ")
         .filter(word => word !== "");
+}
+
+
+export function startREPL() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "> "
+    });
+    
+    rl.prompt();
+
+    rl.on('line', (input) => {
+        const cleaned = cleanInput(input);
+        if (!cleaned) {
+            rl.prompt();
+            return;
+        }
+        
+    });
 }
